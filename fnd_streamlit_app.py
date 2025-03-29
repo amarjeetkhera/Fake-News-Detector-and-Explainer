019ed28116f114ceec4124143d16f03021751c27
Amarjeet Khera <61471405+amarjeetkhera@users.noreply.github.com>
2025-03-29 23:05:52 +0100

    Update fnd_streamlit_app.py

--- fnd_streamlit_app.py
+++ fnd_streamlit_app.py
@@ -114,19 +114,7 @@ if st.button("Analyze"):
 
         # Display results
         box_class = "fake" if result["prediction"] == "Fake" else "real"
-        st.markdown(f"""
-        <style>
-            .result-box {{
-                background-color: #4A4A4A; /* Dark Grey */
-                color: white; 
-                padding: 15px;
-                border-radius: 10px;
-                margin-top: 10px;
-            }}
-            .big-font {{
-                font-size: 18px;
-            }}
-         <style>   
+        st.markdown(f"""  
         <div class="result-box {box_class}">
             <h3>Result: <span style='color: {"red" if result["prediction"] == "Fake" else "green"}'>{result["prediction"]}</span></h3>
             <p class="big-font">Confidence: <b>{result['confidence']:.1%}</b></p>
